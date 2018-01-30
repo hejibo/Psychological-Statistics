622323d8e5e2bc2e8107349b418893db22ba5884
Jibo@macbook pro <hejibo@live.com>
2018-01-30 00:03:17 -0600

    update on lecture 3-measure of dispersions

---
+++ Lecture 3- Measure of Dispersion/Lecture 3. R. mean, SD, variance.R
@@ -0,0 +1,17 @@
+#Lecture 3. R. mean, SD, variance
+
+# assign data values
+values <- c(1, 2, 3, 4) 
+
+mean(values)
+
+median(values)
+
+length(values)
+
+sd(values)
+
+var(values)
+
+range(values)
+
